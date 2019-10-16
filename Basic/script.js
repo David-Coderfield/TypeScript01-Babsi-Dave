06ee1f4b790f84f7d063db5c9adcc4437beb036b
David Riediger <x128@gmx.at>
2019-10-16 07:58:50 +0200

    visual improvement for basic ex

--- Basic/script.js
+++ Basic/script.js
@@ -1,6 +1,8 @@
 var digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// alternate way of defining type:
 // const digitsB : Array<number> = [1,2,3,4,5,6,7,8,9,10];
-(function () {
+//using normal for loop
+function multi1() {
     var html = "";
     for (var i = 0; i < digits.length; i++) {
         html += "<tr>";
@@ -10,4 +12,20 @@ var digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         html += "</tr>";
     }
     document.getElementById('demo').innerHTML = html;
-})();
+}
+//using for of (shorter)
+function multi2() {
+    var html = "";
+    for (var _i = 0, digits_1 = digits; _i < digits_1.length; _i++) {
+        var i = digits_1[_i];
+        debugger;
+        html += "<tr>";
+        for (var _a = 0, digits_2 = digits; _a < digits_2.length; _a++) {
+            var j = digits_2[_a];
+            html += "<td>" + i * j + "</td>";
+            debugger;
+        }
+        html += "</tr>";
+    }
+    document.getElementById('demo').innerHTML = html;
+}
--- Basic/script.ts
+++ Basic/script.ts
@@ -1,7 +1,9 @@
 const digits : number[] = [1,2,3,4,5,6,7,8,9,10];
+// alternate way of defining type:
 // const digitsB : Array<number> = [1,2,3,4,5,6,7,8,9,10];
 
-(function(){
+//using normal for loop
+function multi1(){
 	var html : string = "";
 	for (let i = 0; i < digits.length; i++) {
 		html+="<tr>"
@@ -11,5 +13,17 @@ const digits : number[] = [1,2,3,4,5,6,7,8,9,10];
 		html+="</tr>"
 	}
 	document.getElementById('demo').innerHTML = html;
-})();
+}
 
+//using for of (shorter)
+function multi2(){
+	var html : string = "";
+	for(let i of digits){
+		html+="<tr>";
+		for(let j of digits) {
+			html+=`<td>${i*j}</td>`;
+		}
+		html+="</tr>";
+	}
+	document.getElementById('demo').innerHTML = html;
+}
